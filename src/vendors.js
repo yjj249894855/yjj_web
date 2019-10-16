ea4851ae5325e85f7cf68221cd7b28228df149fd
yjj249894855 <249894855@qq.com>
2019-10-16 16:50:41 +0800

    强制https

--- src/vendors.js
+++ src/vendors.js
@@ -3,7 +3,7 @@ import iView from 'iview';
 import VueRouter from 'vue-router';
 import axios from 'axios';
 import env from './config/env';
-if(location.protocol=='http:'&&env&&env=='pro'){
-  let loc_url = location.href.replace('http:','https:');
-  location.replace(loc_url);
+if (location.protocol == 'http:' && env && env == 'pro') {
+    let loc_url = location.href.replace('http:', 'https:');
+    location.replace(loc_url);
 }
